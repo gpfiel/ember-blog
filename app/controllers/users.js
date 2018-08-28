0f7fd6573fddf507f64e7fd1dfec201325009c78
Gabriel Pinheiro Fiel <gpfiel@gmail.com>
2018-08-28 09:27:40 +0100

    Fix console errors

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,54 +1,54 @@
 import Controller from '@ember/controller';
-import { filter } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { A } from '@ember/array';
 
 export default Controller.extend({
 	title: 'Welcome to tutors match',
 	filter: '',
 
-	currentSortingProperties: ["name:asc"],
+	currentSortingProperties: A(["name:asc"]),
   currentSortingProperty: 'name',
   currentSortingDirection: 'asc',
 
-	usersSortedList: Ember.computed.sort('filteredUsers', 'currentSortingProperties'),
+	usersSortedList: computed.sort('filteredUsers', 'currentSortingProperties'),
 
-  filteredUsers: Ember.computed('filter', function() {
-      const filterTerm = this.get('filter');
-      var model = this.get('model');
+  filteredUsers: computed('filter', function() {
+    const filterTerm = this.get('filter');
+    var model = this.get('model');
 
-      var filtered = model.filter( function(user) {
-          return user.get('name').toLowerCase().indexOf(filterTerm) !== -1 || user.get('subject').toLowerCase().indexOf(filterTerm) !== -1;
-      });
+    var filtered = model.filter( function(user) {
+        return user.get('name').toLowerCase().indexOf(filterTerm) !== -1 || user.get('subject').toLowerCase().indexOf(filterTerm) !== -1;
+    });
 
-      return filtered;
+    return filtered;
   }),
 
-  reverse: Ember.computed('currentSortingDirection', function() {
+  reverse: computed('currentSortingDirection', function() {
     if (this.get('currentSortingDirection') == 'asc')
-		 	return true; 
+			return true; 
 		else 
 			return false;
   }),
 
   reverseDirection() {
 		if (this.get('currentSortingDirection') == 'asc')
-		 	return 'desc' 
+			return 'desc' 
 		else 
 			return 'asc'	
 	},
 
   actions: {
-	  sortBy(property) {
-	  	var direction;
-	  	if (this.get('currentSortingProperty') == property)
-	      direction = this.reverseDirection()
-	    else
-	      direction = 'asc'
+		sortBy(property) {
+			var direction;
+			if (this.get('currentSortingProperty') == property)
+				direction = this.reverseDirection()
+			else
+				direction = 'asc'
 
-	    this.set('currentSortingProperty', property)
-	    this.set('currentSortingDirection', direction)
-	    this.set('currentSortingProperties', [property+":"+direction])
-	  }
-	    
+			this.set('currentSortingProperty', property)
+			this.set('currentSortingDirection', direction)
+			this.set('currentSortingProperties', [property+":"+direction])
+		}
   }
 
 });
